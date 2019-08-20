6015719ad579098e2f8f68c653835587b5002b10
ws <wohshon@gmail.com>
2019-08-20 10:30:57 +0800

    v1

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var ip = require("ip");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+   res.render('index.html', { title: 'Hello Demo!', content: 'Containerized nodejs application', ipaddr: 'served from: '+ip.address() });
+});
+
+module.exports = router;
