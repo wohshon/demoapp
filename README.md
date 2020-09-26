0a9cf2102ffb14c9bd376232cd38b14c5cdcb55f
wohshon <wohshon@gmail.com>
2020-09-26 14:58:07 +0800

    clean up

---
+++ README.md
@@ -0,0 +1,13 @@
+### sample rest api for testing 
+
+- start service : `npm start`
+
+Endpoints:
+
+- http://<host>:<port>/ 
+  
+  returns a webpage
+
+- http://<host>:<port>/hello/<sg>
+
+  echo message
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,10 @@ router.get('/', function(req, res, next) {
    res.render('index.html', { title: 'Hello World!', content: 'Containerized nodejs application', ipaddr: 'served from: '+ip.address() });
 });
 
+router.get('/hello/:msg', function(req, res, next) {
+   var result = {};
+   result["msg"] = req.params.msg;
+   res.send(result);
+});
+
 module.exports = router;
